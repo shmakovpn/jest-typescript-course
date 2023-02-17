93cde9c657727193f310828a3eb207b76ba00b68
shmakovpn <pavel.shmakovpn@zyfra.com>
2023-02-17 13:18:30 +0300

    jest typescript works

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,4 +1,6 @@
 // src/index.spec.ts
 describe('We can', () => { // describe описывает группу из нескольких тестов
-  test('write test', () => {}); // test собственно сам тест
+  test('write test', () => {
+    const hello: string = 'world';  // добавим typescript код
+  }); // test собственно сам тест
 });
