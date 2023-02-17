1be62ee27ccdce0386c05aff5d85487439d2bdc9
shmakovpn <pavel.shmakovpn@zyfra.com>
2023-02-17 12:45:10 +0300

    first jest empty test

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,19 +1,4 @@
 // src/index.spec.ts
-const argsHistory: any[] = []; //
-const print = console.log; // надо оставить нашему тесту возможно вывода на терминал
-const resultFixture = 'Hello World from typescript'; // ожидаемый результат
-// делаем monkey patch
-console.log = (...args) => {
-  argsHistory.push(args);
-};
-
-import './index'; // импортируем тестируемый файл
-
-if (argsHistory.length === 1 && argsHistory[0].length === 1 && argsHistory[0][0] === resultFixture) {
-  // argsHistory.length === 1 проверяем, что импортируемый код вызывал console.log 1 раз
-  // в вызов console.log передан 1 аргумент
-  // argsHistory[0][0] === resultFixture этот аргумент соответствует ожидаемому значению
-  print('Test OK');
-} else {
-  print(`Test Failed, argsHistory="${argsHistory}"`);
-}
+describe('We can', () => { // describe описывает группу из нескольких тестов
+  test('write test', () => {}); // test собственно сам тест
+});
